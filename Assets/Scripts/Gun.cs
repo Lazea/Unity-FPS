9c7be0f9b490e2a6479d39b5b970287470b49a28
Alex-PC\Alex <lazeaai@gmail.com>
2018-06-30 02:37:03 -0400

    Added gun class

---
+++ Assets/Scripts/Gun.cs
@@ -0,0 +1,100 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Gun : MonoBehaviour {
+
+    public int damage = 10;
+
+    public Projectile projectile;
+
+    public int ammoCapacity = 5;
+    int ammo = 0;
+
+    public bool auto;
+    public float fireRate = 0.1f;
+    float fireTime = 0f;
+    bool firing = false;
+
+    public float reloadRate = 3f;
+    float reloadTime = 0f;
+    bool reloading = false;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        // Input
+        bool fire = Input.GetMouseButtonDown(0);
+        if (auto)
+        {
+            fire = Input.GetMouseButton(0);
+        }
+
+        bool reload = Input.GetKeyDown(KeyCode.R);
+
+        // Reload and fire logic
+        if (reloading)
+        {
+            firing = false;
+
+            reloadTime += Time.deltaTime;
+            if(reloadTime >= reloadRate)
+            {
+                reloadTime = 0f;
+                reloading = false;
+
+                ammo = ammoCapacity;
+            }
+        } else if (firing)
+        {
+            reloading = false;
+
+            fireTime += Time.deltaTime;
+            if(fireTime >= fireRate)
+            {
+                fireTime = 0f;
+                firing = false;
+            }
+        } else
+        {
+            if (reload)
+            {
+                Reload();
+            }
+
+            if (fire)
+            {
+                if (ammo > 0)
+                {
+                    Fire();
+                }
+                else
+                {
+                    Reload();
+                }
+            }
+        }
+	}
+
+    void Fire()
+    {
+        firing = true;
+        reloading = false;
+
+        ammo -= 1;
+
+        // TODO: Play fire animation
+    }
+
+    void Reload()
+    {
+        reloading = true;
+        firing = false;
+
+        // TODO: Play reload animation
+    }
+}
---
+++ Assets/Scripts/Projectile.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Projectile : MonoBehaviour {
+
+    public float speed = 100f;
+
+    Vector3 previousPos;
+
+    public float lifeTime = 5f;
+    float elapsedTime = 0f;
+
+	// Use this for initialization
+	void Start () {
+        previousPos = transform.position;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        float tm = Time.deltaTime;
+        transform.position += transform.forward * speed * tm;
+
+        Vector3 disp = previousPos - transform.position;
+        Vector3 dir = disp.normalized;
+        float distance = disp.magnitude;
+        Ray ray = new Ray(transform.position, dir);
+        if (Physics.Raycast(ray, distance))
+        {
+            Hit();
+        }
+
+        elapsedTime += tm;
+        if(elapsedTime >= lifeTime)
+        {
+            DestroyProjectile();
+        }
+
+        previousPos = transform.position;
+    }
+
+    void Hit()
+    {
+        DestroyProjectile();
+    }
+
+    void DestroyProjectile()
+    {
+        Destroy(gameObject);
+    }
+}
